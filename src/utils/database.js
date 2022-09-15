faeb091db420107dd4c6dd5ffa0984792f7f1cd7
MarcoAnyeloCazarez <anyelocaba7@gmail.com>
2022-09-15 01:18:21 -0500

    variables de entorno modificadas

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,14 +1,31 @@
 const { Sequelize } = require('sequelize')  //la variable Sequelize hace referencia a que esta cargada la librería
+const dotenv = require('dotenv')
+dotenv.config({path: './config.env'})
 
 const db = new Sequelize({
     dialect: 'postgres',     //lo pdemos cambiar por el dialecto que se quiera usar, eje: mysql
     
     //ahora las credenciales para las bases de datos
-    host: 'localhost',
+   /* host: 'localhost',
     username: 'postgres',
     password: '1602',
     database: 'airbnb',
-    port: 5432  //pueto en el que se corre por defecto una base de datos
+    port: 5432 */ //pueto en el que se corre por defecto una base de datos
+
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB,
+    logging: false,
+    dialectOptions:
+        process.env.NODE_ENV === 'production'
+        ? {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+            },
+        }
+        : {},
 }) 
 
 module.exports = {
